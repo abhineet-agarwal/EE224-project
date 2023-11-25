f3ec97bb3e872ba8290b3b62dd23f3f526f675d8
Abhineet Agarwal <70828805+abhineet-agarwal@users.noreply.github.com>
2023-11-25 18:25:02 +0530

    Update register1.vhd

--- register1.vhd
+++ register1.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity register1 is
+entity register is
         port(Clk        : in  std_logic;
 			 Reset      : in  std_logic;
              data_in    : in  std_logic_vector(15 downto 0);
